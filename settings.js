0323c77366ef012fecdcb5b7c7af3cbd68248cad
Kingshana0 <liyanaarachchikeshara2@gmail.com>
2024-10-11 17:58:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=3NdUxYLK#6hfVYL9UkGfGUvWQCjqJVCyiYDri18wmLrfSF8f-Fd0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=rEtgHQYA#x-1c6e7pn7jO_dGz9-XQGjCH3C6JLR6TbUwYayVEtZ4' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94717120796' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
